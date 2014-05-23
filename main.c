215cb1225a8aff5ad536365ca0e92a206ed7f971
Svetislav <svetislavv@gnail.com>
2014-05-23 16:26:11 +0200

    Implementirao racunanje povrsine kruga.

--- main.c
+++ main.c
@@ -4,19 +4,31 @@
 int main() {
   printf("Sta hoces da radis?\n");
   printf("Unesi 1 da izracunas povrsinu kvadrata.");
+  printf("Unesi 2 da izracunas povrsinu kruga.");
 
   int unos;
 
   scanf("%d", &unos);
   printf("Uneo si %d.", unos);
-  if (unos == 1) {
+  switch(unos) {
+  case 1:
+
     printf("Unesi stranicu kvadrata: ");
     double stranica;
     scanf("%lf", &stranica);
     
     double povrsina = izracunaj_povrsinu_kvadrata(stranica);
     printf("Povrsina je %lf.\n", povrsina);
+    break;
+  case 2:
+    printf("Unesi precnik kruga:: ");
+    double precnik;
+    scanf("%lf", &precnik);
+    double povrsina = izracunaj_povrsinu_kruga(precnik);
+    printf("Povrsina je %lf.\n", povrsina);
+    break;  
   }
+  
   
 
   return 0;
--- proracun.c
+++ proracun.c
@@ -1,5 +1,11 @@
 
 
+const double PI = 3.14156;
+
 double izracunaj_povrsinu_kvadrata(double a) {
   return a * a;
+}
+
+double izracunaj_povrsinu_kruga(double r) {
+  return PI* r * r;
 }
