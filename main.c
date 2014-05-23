1ea2ae327b5ed6f59397fb128fe2d496daae5f7c
Dimitrije Radojevic <dimitrijer@nordeus.eu>
2014-05-23 15:47:55 +0200

    Implementirao unos od korisnika.

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 
 
 int main() {
+  printf("Sta hoces da radis?\n");
+  printf("Unesi 1 da izracunas povrsinu kvadrata.");
+
+  int unos;
+
+  scanf("%d", &unos);
+  printf("Uneo si %d.", unos);
+
   return 0;
 }
