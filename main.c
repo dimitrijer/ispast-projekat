34388567fe9b51d6e9146c6e3409633acfcc6ced
Dimitrije Radojevic <dimitrijer@nordeus.eu>
2014-05-23 16:34:43 +0200

    Ispravljam Svetislavljevo sr***e

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 int main() {
   printf("Sta hoces da radis?\n");
-  printf("Unesi 1 da izracunas povrsinu kvadrata.");
-  printf("Unesi 2 da izracunas povrsinu kruga.");
+  printf("Unesi 1 da izracunas povrsinu kvadrata.\n");
+  printf("Unesi 2 da izracunas povrsinu kruga.\n");
 
   int unos;
 
@@ -12,20 +12,23 @@ int main() {
   printf("Uneo si %d.", unos);
   switch(unos) {
   case 1:
-
+    {
     printf("Unesi stranicu kvadrata: ");
     double stranica;
     scanf("%lf", &stranica);
     
     double povrsina = izracunaj_povrsinu_kvadrata(stranica);
     printf("Povrsina je %lf.\n", povrsina);
+    }
     break;
   case 2:
-    printf("Unesi precnik kruga:: ");
+    {
+    printf("Unesi precnik kruga: ");
     double precnik;
     scanf("%lf", &precnik);
     double povrsina = izracunaj_povrsinu_kruga(precnik);
     printf("Povrsina je %lf.\n", povrsina);
+    }
     break;  
   }
   
--- proracun.h
+++ proracun.h
@@ -2,5 +2,5 @@
 #define __PRORACUN_H__
 
 double izracunaj_povrsinu_kvadrata(double a);
-
+double izracunaj_povrsinu_kruga(double r);
 #endif
